5c7cc226f82c83768def13b22783bd87b11427f7
hiro3dayu <64936517+hiro3dayu@users.noreply.github.com>
2020-05-11 15:49:39 +0900

    Q004完了

--- src/main/java/q004/Q004.java
+++ src/main/java/q004/Q004.java
@@ -15,5 +15,19 @@ package q004;
  * - ListManagerクラスの比較 compare と入れ替え exchange を使って実現してください
  */
 public class Q004 {
+    public static void main(String[] args) {
+        ListManager data = new ListManager();
+        // TODO 並び換え
+        int s = data.size();
+        System.out.print(s + "\n");
+        for (int j = 0; j < data.size();j++) {
+            for (int i = 1; i < data.size() - j; i++) {
+                if (data.compare(i - 1, i) == 1) {
+                    data.exchange(i - 1, i);
+                }
+            }
+        }
+        data.checkResult();
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 03時間 00分
